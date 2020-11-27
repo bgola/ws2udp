28e703911330f976f64f575401163c3b1702c0d9
Bruno Gola <me@bgo.la>
2020-11-27 12:12:07 +0100

    adds --quiet cmdline option

--- ws2udp/__main__.py
+++ ws2udp/__main__.py
@@ -7,7 +7,6 @@ import argparse
 import logging
 
 def main():
-    logging.basicConfig(level=logging.INFO, format="%(message)s")
     parser = argparse.ArgumentParser(
         description="WebSocket to UDP proxy"
         )
@@ -15,11 +14,21 @@ def main():
     parser.add_argument("--udp-port", default="57142", help="Port of the UDP receiver (default=57142)")
     parser.add_argument("--addr", default="0.0.0.0", help="WebSocket address to listen (default=0.0.0.0)")
     parser.add_argument("--port", default=8765, help="WebSocket port to listen (default=8765)")
+    parser.add_argument("--quiet", action='store_true', help="No verbose output")
     
     args = parser.parse_args()
+
+    if args.quiet:
+        logging.basicConfig(level=logging.WARNING, format="%(message)s")
+    else:
+        logging.basicConfig(level=logging.INFO, format="%(message)s")
+
     udp_addr = (args.udp_addr, int(args.udp_port))
     logging.info(f"Forwarding WebSocket messages received on {args.addr}:{args.port}")
     logging.info(f"Broadcasting UDP messages received on {args.udp_addr}:{args.udp_port}")
 
-    asyncio.run(run(udp_addr, args.addr, args.port))
-    asyncio.get_event_loop().run_forever()
+    try:
+        asyncio.run(run(udp_addr, args.addr, args.port))
+        asyncio.get_event_loop().run_forever()
+    except KeyboardInterrupt:
+        logging.info("Quitting...")
